a10eda4808bff65e272db4aa936389572416a11a
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-05-11 19:31:22 -0500

    starfish added

--- app/public/components/mainContent.js
+++ app/public/components/mainContent.js
@@ -7,6 +7,18 @@
     .component('mainContent', {
       controller: ['$scope', '$http', function ($scope, $http) {
         const vm = this;
+        vm.$onInit = function () {
+          vm.pipe = '../images/pipe.png'
+          vm.starry = '../images/starry.png'
+        }
+        vm.starfish = function () {
+          vm.pipe = '../images/pipstar.png'
+          vm.starry = '../images/nostarry.png'
+        }
+        vm.morestar = function () {
+          vm.pipe = '../images/pipe.png'
+          vm.starry = '../images/starry.png'
+        }
       }],
       templateUrl: './components/mainContent.html',
     });
